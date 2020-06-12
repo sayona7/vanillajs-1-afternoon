5d87426673ca990ce41fea1601da41e8521b1f2f
Veronica <weronika.bakanowicz@g.austincc.edu>
2020-06-12 09:12:03 -0500

    cleaned a bit of code and linked the function

--- Part1/index.js
+++ Part1/index.js
@@ -2,7 +2,22 @@
 Vanilla Project
 Author: Veronica Bakanowicz */
 
+/* Variable Declaration */
+
 const arrayBoard = ["", "", "", "", "", "", "", "", ""];
+
+const winning = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+]
+
+/* Board tracking, Winning and Tie */
 
 function array() {
     for (i = 0; i < 9; i++) {
@@ -12,9 +27,28 @@ function array() {
             arrayBoard.splice(i, 1, document.getElementById(i).innerText);
         }
     }
+
+    let winner = null;
+    winning.forEach((combo, index) => {
+        if (arrayBoard[combo[0]] && arrayBoard[combo[0]] === arrayBoard[combo[1]] && arrayBoard[combo[0]] === arrayBoard[combo[2]]) {
+            winner = arrayBoard[combo[0]];
+            console.log(winner);
+        }
+    });
+
+    if (winner) {
+        console.log(winner);
+    } else if (arrayBoard.includes("")) {
+        console.log(null);
+    } else {
+        alert("Cat's game, there is no winner.");
+        clear();
+    }
+
     console.log(arrayBoard);
 }
 
+/* Main Function */
 
 function play(id) {
     const player = document.getElementById("player");
@@ -31,38 +65,16 @@ function play(id) {
     } else {
         console.log("can't change declared square");
     }
-    
-}
-
-const winning = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-]
-
-function determineWinner(arr) {
-
-
 }
 
 
-
-// Alert winner 
-
-// if (winner = true) ? alert("`The winner is player ${}") : alert("Cat's game, there is no winner");
-
+/* Resetting anf Event Listener */
 
 function clear() {
     for (let i = 0; i <= 8; i++) {
-    document.getElementById(i).innerText = "";
-    arrayBoard[i] = "";
+        document.getElementById(i).innerText = "";
+        arrayBoard[i] = "";
     }
-
 }
 
 document.getElementById("reset-btn").addEventListener("click", clear);
